bf1cf53137284ec6975a4cc2ec7ca3cd204abc5d
SakshamKhatwani <sakshamkhatwani@gmail.com>
2019-12-26 15:03:06 +0530

    initial commit

---
+++ src/component/myComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Loader extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            cursor: 0,
+            loadedItems: [],
+            flag: 0
+        };
+        this.handleNext = this.handleNext.bind(this);
+        this.handleFetch = this.handleFetch.bind(this);
+    }
+
+    handleFetch(){
+        const promiseList = this.props.render();
+        Promise.all(promiseList.map(p => 
+            p.catch(() => this.props.err))).then((values) =>
+            {
+                this.setState((state) => ({
+                    loadedItems: state.loadedItems.concat(values),
+                    flag: 1
+                }));
+            }).catch((err)=>{
+                console.log("unable to load");
+            })
+    }
+    componentDidMount(){
+        this.handleFetch();
+    }
+
+    handleNext(){
+        console.log("this is clicked");
+        this.handleFetch();
+        console.log(this.state.loadedItems);
+        this.setState((state) => ({
+            cursor: state.cursor + this.props.n
+        }));
+        
+    }
+    render(){
+    const showList = this.state.loadedItems.slice(
+        this.state.cursor, this.state.cursor + this.props.n).map((value, index) => <li key={index}>{value}</li>);
+        return(
+            <div>
+                <h1>I am here</h1>
+                <h1>{this.flag}</h1>
+                <ul>{showList}</ul>
+                <button onClick={this.handleNext}>Next</button>
+            </div>
+        );
+    }
+}
+
+export default Loader;
---
+++ src/playground.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Loader from './component/myComponent';
+
+class TempComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            listOfPromises: [Promise.resolve(1), new Promise((resolve, reject) => {
+                setTimeout(resolve, 5000, 'one');
+            }), Promise.resolve(3)]
+        };
+    }
+
+    render(){
+
+        return(
+            <div>
+                <h1>this is being done</h1>
+                <Loader n={3} render={() => this.state.listOfPromises} err={null}/>
+            </div>
+        );
+    }
+}
+
+export default TempComponent;
